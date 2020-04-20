ee6975fb871ec1d39f732b8edbddb29d65ab3311
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-04-20 00:49:19 -0400

    pushing up for Rog

--- project7app/src/Body.js
+++ project7app/src/Body.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Body extends Component {
-    
-    componentDidMount() {
-        console.log('mounted')
-    }
-    
-    render() {
-        return (
-            <div className="body">
-                <h1>{this.props.word}</h1>
-                <p>{this.props.rhymes}</p>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      display: "",
+    };
+
+  }
+
+  componentDidMount() {
+    let URL =
+      "https://cors-anywhere.herokuapp.com/https://rebeccas-rhymes-api.herokuapp.com/list/word/cup";
+    fetch(URL)
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          this.setState({
+            display: res,
+          }),
+        console.log(this.state.display[0])
+        });
+  }
+
+//   componentDidMount() {
+//     this.grabWord();
+//     console.log("component has mounted");
+//   }
+
+  render() {
+    return (
+      <div className="body">
+        {/* <h1>{this.state.display.word}</h1> */}
+        {/* <p>{this.state.display}</p> */}
+      </div>
+    );
+  }
 }
 
 export default Body;
