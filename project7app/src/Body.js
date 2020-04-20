10ec668ecf9080df2b540abc6ca7021aed4464d4
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-04-20 01:12:08 -0400

    hard coded rhymes rendering

--- project7app/src/Body.js
+++ project7app/src/Body.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-
 class Body extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      display: "",
+      display: "roger",
+      allRhymes: []
     };
-
   }
-
   componentDidMount() {
     let URL =
       "https://cors-anywhere.herokuapp.com/https://rebeccas-rhymes-api.herokuapp.com/list/word/cup";
@@ -17,26 +14,32 @@ class Body extends Component {
       .then((res) => res.json())
       .then(
         (res) => {
+          console.log(res)
           this.setState({
             display: res,
-          }),
-        console.log(this.state.display[0])
+            allRhymes: res[0].rhymes.all
+          })
+          // console.log(this.state.display[0].rhymes.all)
         });
   }
-
-//   componentDidMount() {
-//     this.grabWord();
-//     console.log("component has mounted");
-//   }
-
+  //   componentDidMount() {
+    //   this.grabWord();
+  //     console.log("component has mounted");
+  //   }
   render() {
+    // console.log(this.state.display[0].rhymes.all)
+    let { allRhymes, display } = this.state
+    let list = allRhymes.map((word, i) => {
+      return <p key={i}>{word}</p>
+    })
+    // console.log(this.state.display)
     return (
       <div className="body">
+        {list}
         {/* <h1>{this.state.display.word}</h1> */}
         {/* <p>{this.state.display}</p> */}
       </div>
     );
   }
 }
-
 export default Body;
